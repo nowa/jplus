67dd64a69b0cd9beff13694552435f2a1d10a2b9
nowa <nowazhu@gmail.com>
2008-06-19 13:30:08 +0800

    add notes to base.js

--- src/core/base.js
+++ src/core/base.js
@@ -82,6 +82,15 @@ Object.extend = function(destination, source) {
 // 向Object添加自定义方法
 Object.extend(Object, {
 	
+	/**
+	 * 返回一个数组，其中包含了一个对象中的全部现有的主键
+	 *
+	 * @author from Prototype1.6
+	 * @class Object
+	 * @method keys
+	 * @param {Object} object
+	 * @return {Array} property key array
+	 */
 	keys: function(object) {
     var keys = [];
     for (var property in object)
@@ -194,12 +203,30 @@ Object.extend(Object, {
 
 Object.extend(Function.prototype, {
 	
+	/**
+	 * 返回参数的数组列表
+	 *
+	 * @author from Prototype1.6
+	 * @class Function
+	 * @method argumentNames
+	 * @return {Array} arguments array
+	 */
 	argumentNames: function() {
     var names = this.toString().match(/^[\s\(]*function[^(]*\(([^\)]*)\)/)[1]
       .replace(/\s+/g, '').split(',');
     return names.length == 1 && !names[0] ? [] : names;
   },
 
+	/**
+	 * bind不会让this指针被劫持，它用于绑定上下文（this）
+	 * 或者也可以说让一个没有m方法的b对象具备m方法： a.m.bind(b)
+	 * 和call、apply有相同之处，但是可以传递两次参数
+	 *
+	 * @author from Prototype1.6
+	 * @class Function
+	 * @method bind
+	 * @return {Function} function
+	 */
 	bind: function() {
     if (arguments.length < 2 && Object.isUndefined(arguments[0])) return this;
     var __method = this, args = $A(arguments), object = args.shift();
@@ -208,10 +235,18 @@ Object.extend(Function.prototype, {
     }
   },
 
+	/**
+	 * 使用指定的function作为wrapper来包装自身
+	 *
+	 * @author from Prototype1.6
+	 * @class Function
+	 * @method wrap
+	 * @return {Function} function
+	 */
 	wrap: function(wrapper) {
     var __method = this;
     return function() {
-      return wrapper.apply(this, [__method.bind(this)].concat($A(arguments))); 
+      return wrapper.apply(this, [__method.bind(this)].concat($A(arguments))); 		// 之所以要bind是为了将this传递到wrapper里去，否则是不具备对当前对象的引用的
     }
   },
 	
